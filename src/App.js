f85a1ded0f5ac033fe929ccef93beb6d4974fd17
adershgr <mail4me.adersh@gail.com>
2023-03-11 02:37:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header className="header">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xs-3 col-lg-2">
+              <div className="header__logo">
+                <h3>E-Store</h3>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-7">
+              <div className="input-group">
+                <div className='input-group-prepend' >
+                  <select className='form-control btn btn-success dropdown-toggle'>
+                    <option>ALL </option>
+                    <option>Men </option>
+                    <option>Women</option>
+                    <option>Kids</option>
+                  </select>
+                </div>
+
+                <input type="text" className="form-control">
+                </input>
+
+                <div className="input-group-append">
+                  <button className="btn btn-success" >
+                    <i className="fa fa-search"></i>
+                  </button>
+                </div>
+              </div>
+           
+            </div>
+            <div className="col-xl-4 col-lg-2">
+            <div className='header__right'>
+              <div className='auth'>
+                <a href="#"> Sign-In /</a>
+                <a href="#"> Sign-Up </a>
+              </div>
+   <ul className='widget'>
+   <li><i className='fa fa-heart' /></li>
+   <li><i className='fa fa-shopping-cart' /></li>
+   </ul>
+            </div>
+            </div>
+
+          </div>
+        </div>
       </header>
     </div>
   );
